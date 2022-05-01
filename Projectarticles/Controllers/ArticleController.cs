bf596dc14e1d29957822a24b86166c15c3edb846
cary-cyon <kozvlad99@gmail.com>
2022-05-01 15:38:30 +0300

    Initial create

---
+++ Projectarticles/Controllers/ArticleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projectarticles.Controllers
+{
+    
+    public class ArticleController : Controller
+    {
+        [Route("/article")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projectarticles/Models/Articles/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace Projectarticles.Models.Articles
+{
+    public class Article
+    {
+        
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Author { get; set; }
+        
+        public string Text { get; set; }
+
+    }
+}
